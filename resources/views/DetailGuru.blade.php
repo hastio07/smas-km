3c9e39208f376061e9983961545ac44dacd70dcb
Akhbarona <akhbarona.118140193@student.itera.ac.id>
2023-12-26 20:53:08 +0700

    doc: pindah file

--- resources/views/DetailGuru.blade.php
+++ resources/views/DetailGuru.blade.php
@@ -1,4 +1,4 @@
-@extends('user.layouts.layoutUser')
+@extends('layouts.default')
 
 @section('title', 'PPDB')
 
@@ -11,7 +11,7 @@
         <div class="container my-5 shadow">
             <div class="row">
                 <div class="col-md-8">
-                    <h5 class="text-center fw-bold my-5">Biodata Guru</h5>
+                    <h5 class="fw-bold my-5 text-center">Biodata Guru</h5>
                     <div class="row p-3">
                         <div class="col-md-7">
                             <img alt="..." class="img-thumbnail d-block mx-auto mb-3" src="{{ asset('assets/image/guru/guru3.jpg') }}" style="max-width: 270px; max-height: 290px;">
@@ -57,28 +57,28 @@
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <div class="px-3 border rounded my-4">
-                        <h5 class="text-center fw-bold mb-5 my-4">Berita Terbaru</h5>
-                        <a href="" class="text-decoration-none">
-                            <div class="card border-0 shadow mb-2">
+                    <div class="my-4 rounded border px-3">
+                        <h5 class="fw-bold my-4 mb-5 text-center">Berita Terbaru</h5>
+                        <a class="text-decoration-none" href="">
+                            <div class="card mb-2 border-0 shadow">
                                 <div class="card-body d-flex">
                                     <img alt="..." class="img-thumbnail me-2" src="{{ asset('assets/image/guru/guru3.jpg') }}" style="max-width: 70px; max-height: 90px;">
                                     <div class="card-body p-0">
                                         <p class="card-title fw-bold">Prestasi Siswa Kelas XII</p>
                                         <p class="card-text mb-0" style="font-size: small">With supporting text below as a natural lead-in to additional content.</p>
-                                        <p class="mb-0 mt-1 fst-italic text-muted" style="font-size: small"><i class="bi bi-calendar-event-fill"></i> 20-02-2024</p>
+                                        <p class="fst-italic text-muted mb-0 mt-1" style="font-size: small"><i class="bi bi-calendar-event-fill"></i> 20-02-2024</p>
                                     </div>
                                 </div>
                             </div>
                         </a>
-                        <a href="" class="text-decoration-none">
-                            <div class="card border-0 shadow mb-2">
+                        <a class="text-decoration-none" href="">
+                            <div class="card mb-2 border-0 shadow">
                                 <div class="card-body d-flex">
                                     <img alt="..." class="img-thumbnail me-2" src="{{ asset('assets/image/guru/guru3.jpg') }}" style="max-width: 70px; max-height: 90px;">
                                     <div class="card-body p-0">
                                         <p class="card-title fw-bold">Antusias Class Meeting</p>
                                         <p class="card-text mb-0" style="font-size: small">With supporting text below as a natural lead-in to additional content.</p>
-                                        <p class="mb-0 mt-1 fst-italic text-muted" style="font-size: small"><i class="bi bi-calendar-event-fill"></i> 20-02-2024</p>
+                                        <p class="fst-italic text-muted mb-0 mt-1" style="font-size: small"><i class="bi bi-calendar-event-fill"></i> 20-02-2024</p>
                                     </div>
                                 </div>
                             </div>
