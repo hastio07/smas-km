4187f40ec3d33e75c182e03120418a1d4a225b54
hastio07 <hastiowahyuu@gmail.com>
2024-01-02 19:56:42 +0700

    menambahkan dan menampilkan carousel pada setiap page

--- resources/views/main/layout/includes/carousel.blade.php
+++ resources/views/main/layout/includes/carousel.blade.php
@@ -1,4 +1,4 @@
-<div class="carousel slide rounded mt-3" id="carousel-sambutan">
+<div class="carousel slide mt-3" id="carousel-sambutan">
     <div class="carousel-indicators">
         <button aria-current="true" aria-label="Slide 1" class="active" data-bs-slide-to="0" data-bs-target="#carousel-sambutan" type="button"></button>
         <button aria-label="Slide 2" data-bs-slide-to="1" data-bs-target="#carousel-sambutan" type="button"></button>
@@ -6,14 +6,21 @@
     </div>
     <div class="carousel-inner">
         <div class="carousel-item active" data-bs-interval="5000">
-            <img alt="..." class="mh-100 object-fit-cover rounded-4 d-block w-100" height="900" src="{{ asset('assets/image/carousel/carousel.jpeg') }}">
+            <img alt="..." class="mh-100 object-fit-cover d-block w-100" height="900" src="{{ asset('assets/image/carousel/carousel.jpeg') }}">
             <div class="carousel-caption d-none d-md-block">
                 <h5>First slide label</h5>
                 <p>Some representative placeholder content for the first slide.</p>
             </div>
         </div>
         <div class="carousel-item" data-bs-interval="5000">
-            <img alt="..." class="mh-100 object-fit-cover rounded-4 d-block w-100" height="900" src="{{ asset('assets/image/carousel/carousel2.png') }}">
+            <img alt="..." class="mh-100 object-fit-cover d-block w-100" height="900" src="{{ asset('assets/image/carousel/carousel2.png') }}">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+            </div>
+        </div>
+        <div class="carousel-item" data-bs-interval="5000">
+            <img alt="..." class="mh-100 object-fit-cover d-block w-100" height="900" src="{{ asset('assets/image/PPDB/daftar2.jpg') }}">
             <div class="carousel-caption d-none d-md-block">
                 <h5>Second slide label</h5>
                 <p>Some representative placeholder content for the second slide.</p>
